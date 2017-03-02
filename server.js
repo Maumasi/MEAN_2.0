0a8a216b3e50d8534b37bfb6203f96ed87e674f2
maumasi <liu.maumasi@gmail.com>
2017-03-02 18:59:52 -0500

    building node server file

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const routes = require('./src/routes/');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/', routes(express));
+
+app.use(express.static('./public'));
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+module.exports = server;
