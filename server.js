92f299341ca725329a2ae678c03c4b086d753ceb
maumasi <liu.maumasi@gmail.com>
2017-03-09 15:26:58 -0500

    fixed .env to work in the unit tests

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ dotenv.load();
 
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+let port = process.env.PORT || 3000;
+
+// change port for testing so default dev port 3000 is still open
+if (process.env.NODE_ENV === 'test') {
+  port = process.env.TEST_PORT;
+}
+
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = server;
--- test/_test_task_routes.js
+++ test/_test_task_routes.js
@@ -1,5 +1,11 @@
 
+// load the `.env` file
+const dotenv = require('dotenv');
+dotenv.load();
+
 const assert = require('assert');
+const request = require('supertest');
+const app = require('../src/app');
 
 describe('Task endpoints', () => {
   it('create: /todo/v1/task/add', (done) => {
@@ -7,22 +13,27 @@ describe('Task endpoints', () => {
     done();
   });
 
-  it('update: /todo/v1/task/add', (done) => {
+  it('update: /todo/v1/task/edit/:id', (done) => {
     assert(1 + 1 === 2);
     done();
   });
 
-  it('find by id: /todo/v1/task/add', (done) => {
+  it('find by id: /todo/v1/task/:id', (done) => {
     assert(1 + 1 === 2);
     done();
   });
 
-  it('find all: /todo/v1/task/add', (done) => {
-    assert(1 + 1 === 2);
-    done();
+  it('find all: /todo/v1/tasks/', (done) => {
+    request(app)
+      .get('/todo/v1/tasks/')
+      .end((error, res) => {
+        done();
+      });
+    // assert(1 + 1 === 2);
+    // done();
   });
 
-  it('delete: /todo/v1/task/add', (done) => {
+  it('delete: /todo/v1/task/remove/:id', (done) => {
     assert(1 + 1 === 2);
     done();
   });
