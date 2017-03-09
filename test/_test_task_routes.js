a237efa0808fb3f36507a1ec13ec603ca47ad540
maumasi <liu.maumasi@gmail.com>
2017-03-09 14:37:47 -0500

    made template tests for for task endpoints

---
+++ test/_test_task_routes.js
@@ -0,0 +1,29 @@
+
+const assert = require('assert');
+
+describe('Task endpoints', () => {
+  it('create: /todo/v1/task/add', (done) => {
+    assert(1 + 1 === 2);
+    done();
+  });
+
+  it('update: /todo/v1/task/add', (done) => {
+    assert(1 + 1 === 2);
+    done();
+  });
+
+  it('find by id: /todo/v1/task/add', (done) => {
+    assert(1 + 1 === 2);
+    done();
+  });
+
+  it('find all: /todo/v1/task/add', (done) => {
+    assert(1 + 1 === 2);
+    done();
+  });
+
+  it('delete: /todo/v1/task/add', (done) => {
+    assert(1 + 1 === 2);
+    done();
+  });
+});
