13a3be04bbe8a72606a0b5cdd56f0b1f8b1786bd
maumasi <liu.maumasi@gmail.com>
2017-03-09 18:28:31 -0500

    unit test for create and find all working

--- test/_test_task_routes.js
+++ test/_test_task_routes.js
@@ -7,16 +7,41 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('../src/app');
 
+const testDescription = 'task unit test for create';
+const testDate = new Date();
+let testId;
+
+
 describe('Task endpoints', () => {
   it('create: /todo/v1/task/add', (done) => {
-    assert(1 + 1 === 2);
-    done();
+    request(app)
+      .post('/todo/v1/task/add')
+      .send({
+        description: testDescription,
+      })
+      .end((error, res) => {
+        const test = res.body;
+        testId = test._id;
+
+        assert(test.description === testDescription);
+        done();
+      });
   });
 
+
   it('update: /todo/v1/task/edit/:id', (done) => {
-    assert(1 + 1 === 2);
-    done();
+    request(app)
+    .put(`/todo/v1/task/edit/${testId}`)
+    .send({
+      finishedAt: testDate,
+    })
+    .end((error, res) => {
+      const test = res.body;
+      assert(test.finishedAt === testDate);
+      done();
+    });
   });
+
 
   it('find by id: /todo/v1/task/:id', (done) => {
     assert(1 + 1 === 2);
@@ -27,11 +52,13 @@ describe('Task endpoints', () => {
     request(app)
       .get('/todo/v1/tasks/')
       .end((error, res) => {
-        done();
+        if (res.body) {
+          assert(res.body.length > 0);
+          done();
+        }
       });
-    // assert(1 + 1 === 2);
-    // done();
   });
+
 
   it('delete: /todo/v1/task/remove/:id', (done) => {
     assert(1 + 1 === 2);
