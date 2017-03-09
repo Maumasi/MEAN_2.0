af6a3ddf86bc321a0c60870faf6aa77cf8f42ee8
maumasi <liu.maumasi@gmail.com>
2017-03-09 13:53:11 -0500

    found the issue. I just forgot to add a needed '/' at the beginning of the route

--- src/routes/endpoints/v1.js
+++ src/routes/endpoints/v1.js
@@ -7,15 +7,15 @@ module.exports = (express) => {
 
   // parent route: /todo/v1
   // ==========================================
-  router.post('task/add', TodoController.create);
-  router.put('task/edit/:id', TodoController.update);
-  router.delete('task/remove/:id', TodoController.delete);
-  router.get('task/:id', TodoController.find.byId);
-  router.get('tasks/', TodoController.find.all);
+  router.post('/task/add', TodoController.create);
+  router.put('/task/edit/:id', TodoController.update);
+  router.delete('/task/remove/:id', TodoController.delete);
+  router.get('/task/:id', TodoController.find.byId);
+  router.get('/tasks', TodoController.find.all);
 
-  router.post('user/add', UserController.create);
-  router.put('user/edit/:id', UserController.update);
-  router.delete('user/remove/:id', UserController.delete);
-  router.get('user/:id', UserController.find.byId);
+  router.post('/user/add', UserController.create);
+  router.put('/user/edit/:id', UserController.update);
+  router.delete('/user/remove/:id', UserController.delete);
+  router.get('/user/:id', UserController.find.byId);
   return router;
 };
